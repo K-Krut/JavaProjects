e06cbcd3b41e547cfcd87bc4a7858fb8dab82e4f
kekekek <90685950+K-Krut@users.noreply.github.com>
2022-04-09 20:36:49 +0300

    Add files via upload

--- Second_Part/Project_2/test/MainTest.java
+++ Second_Part/Project_2/test/MainTest.java
@@ -1,8 +1,5 @@
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -12,17 +9,19 @@ class MainTest {
 
     Rectangular Rectangle = new Rectangular(5, 6, 7, 8);
 
-    ArrayList<String> Expected_constructors = new ArrayList<String>(
-            Arrays.asList("Rectangular(class Rectangular)",
-            "Rectangular()", "Rectangular(double, double)", "Rectangular(double, double, double, double)"));
+//    ArrayList<String> Expected_constructors = new ArrayList<String>(
+//            Arrays.asList("Rectangular(class Rectangular)",
+//            "Rectangular()", "Rectangular(double, double)", "Rectangular(double, double, double, double)"));
 
     ArrayList<String> Expected_methods = new ArrayList<>(Arrays.asList(
             "Rectangular.area() = 56.0", "Rectangular.getHeight() = 7.0", "Rectangular.getWidth() = 8.0",
             "Rectangular.getX() = 5.0", "Rectangular.getY() = 6.0", "Rectangular.perimeter() = 30.0")
     );
-    ArrayList<String> Expected_fields = new ArrayList<>(Arrays.asList(
-            "Rectangular.height = 7.0 type: double modifier: private", "Rectangular.width = 8.0 type: double modifier: private)"
-    );
+
+//    ArrayList<String> Expected_fields = new ArrayList<>(Arrays.asList(
+//            "Rectangular.height = 7.0 type: double modifier: private", "Rectangular.width = 8.0 type: double modifier: private)"
+//    ));
+//
 
     @BeforeAll
     static void setUp() {
@@ -38,47 +37,11 @@ class MainTest {
     }
 
     @Test
-    void getting_class_fields() {
-        try {
-            assertEquals(Main.getting_class_fields(Rectangle), Expected_methods);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Test
-    void getting_annotated_methods() {
-    }
-
-    @Test
-    void getting_method_by_name() {
-    }
-
-    @Test
-    void getting_filed_by_name() {
-    }
-
-    @Test
-    void get_annotations() {
-    }
-
-    @Test
-    void get_constructors() {
-    }
-
-    @Test
-    void get_types() {
-    }
-
-    @Test
-    void testGet_types() {
-    }
-
-    @Test
-    void rectangle_constructor_test() {
-    }
-
-    @Test
     void check_invocation() {
+        try {
+            Main.check_invocation(Rectangle);
+        } catch (Exception e) {
+            assertEquals("java.lang.RuntimeException: Object immutable", e.toString());
+        }
     }
 }
