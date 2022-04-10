ec13843c8800fb9452b92b4f71754e945b293537
kekekek <90685950+K-Krut@users.noreply.github.com>
2022-04-10 21:34:36 +0300

    Add files via upload

---
+++ Second_Part/Project_3/test/ControllerTest.java
@@ -0,0 +1,86 @@
+import org.apache.commons.lang3.StringUtils;
+import org.junit.jupiter.api.Test;
+import org.mockito.*;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+class ControllerTest {
+
+    Controller controller = Mockito.mock(Controller.class);
+    Point_Class point = Mockito.mock(Point_Class.class);
+    Rectangular rectangular = Mockito.mock(Rectangular.class);
+    Rectangular rectangularObject = new Rectangular(9, 9, 9, 9);
+    Point_Class pointObject = new Point_Class(9, 9);
+
+
+    StringBuffer kek = new StringBuffer("kek");
+    StringBuffer kekekek = new StringBuffer("mda");
+
+//    @Test
+//    void proxyObject() {
+//        Mockito.doThrow(new IllegalAccessException()).when(controller).ProxyObject(point);
+//        controller.ProxyObject(point);
+//    }
+
+    @Test
+    void annotationMethodInvoke() {
+        Point_Class point = new Point_Class(1, 1);
+        Mockito.when(controller.AnnotationMethodInvoke(point, Point_Class.class)).thenReturn(1.);
+        assertEquals(controller.AnnotationMethodInvoke(point, Point_Class.class), 1.);
+        controller.AnnotationMethodInvoke(rectangular, Rectangular.class);
+        Mockito.verify(controller).AnnotationMethodInvoke(rectangular, Rectangular.class);
+    }
+
+    @Test
+    void nameClass() {
+        Mockito.when(controller.NameClass(Point_Class.class)).thenReturn("P");
+        assertEquals(controller.NameClass(Point_Class.class), "P");
+        assertNotEquals(controller.NameClass(Point_Class.class), "R");
+        controller.NameClass(Rectangular.class);
+        Mockito.verify(controller).NameClass(Rectangular.class);
+    }
+
+    @Test
+    void gettingClassFields() {
+        Mockito.when(controller.gettingClassFields(rectangularObject)).thenReturn(kek);
+        assertEquals(controller.gettingClassFields(rectangularObject), kek);
+    }
+
+    @Test
+    void gettingClassMethods() throws Exception {
+        Mockito.when(controller.gettingClassMethods(rectangularObject)).thenReturn(kek);
+        assertEquals(controller.gettingClassMethods(rectangularObject), kek);
+    }
+
+    @Test
+    void getAnnotations() throws Exception {
+        Mockito.when(controller.getAnnotations(rectangular)).thenReturn(kek);
+        assertEquals(controller.getAnnotations(rectangular), kek);
+    }
+
+    @Test
+    void gettingClassModifier() {
+        Mockito.when(controller.gettingClassModifier(rectangular)).thenReturn("KEK");
+        assertEquals(controller.gettingClassModifier(rectangular), "KEK");
+    }
+
+    @Test
+    void gettingInterfaces() {
+        Mockito.when(controller.gettingInterfaces(Rectangular.class)).thenReturn(kek);
+        assertEquals(controller.gettingInterfaces(Rectangular.class), kek);
+    }
+
+    @Test
+    void getConstructors() {
+        Mockito.when(controller.getConstructors(rectangularObject)).thenReturn(kek);
+        assertEquals(controller.getConstructors(rectangularObject), kek);
+        assertNotEquals(controller.getConstructors(rectangularObject), kekekek);
+    }
+}
---
+++ Second_Part/Project_3/test/RectangularTest.java
@@ -0,0 +1,100 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class RectangularTest {
+
+    Rectangular TestObject = new Rectangular();
+    Rectangular Rectangle_ = new Rectangular(4, 9, 8.5, 7);
+
+
+
+    @Test
+    void Rectangular() {
+        assertEquals(Rectangle_, new Rectangular(Rectangle_));
+        Rectangular KEK = new Rectangular(4, 5, 6, 8);
+        assertNotEquals(KEK, new Rectangular(Rectangle_));
+    }
+
+
+    @Test
+    void getHeight() {
+        TestObject.setHeight(7.9);
+        assertEquals(7.9, TestObject.getHeight());
+        assertNotEquals(2.9, TestObject.getHeight());
+    }
+
+    @Test
+    void getWidth() {
+        TestObject.setWidth(5);
+        assertEquals(5, TestObject.getWidth());
+        assertNotEquals(2.9, TestObject.getWidth());
+    }
+
+    @Test
+    void setWidth() {
+        TestObject.setWidth(8.9);
+        assertEquals(8.9, TestObject.getWidth());
+        assertNotEquals(2, TestObject.getWidth());
+    }
+
+
+    @Test
+    void setHeight() {
+        TestObject.setHeight(5);
+        assertEquals(5, TestObject.getHeight());
+        assertNotEquals(9, TestObject.getHeight());
+    }
+
+    @Test
+    void area() {
+        assertEquals(59.5, Rectangle_.area());
+        assertNotEquals(64.9, Rectangle_.area());
+    }
+
+    @Test
+    void perimeter() {
+        assertEquals(31, Rectangle_.perimeter());
+        assertNotEquals(69, Rectangle_.perimeter());
+    }
+
+    @Test
+    void isEmpty() {
+        assertFalse(Rectangle_.isEmpty());
+        assertNotEquals(true, Rectangle_.isEmpty());
+    }
+
+    @Test
+    void contains() {
+        assertTrue(Rectangle_.contains(5, 12));
+        assertFalse(Rectangle_.contains(2, 12));
+    }
+
+    @Test
+    void moving() {
+        Rectangle_.moving(1, 3);
+        assertEquals(new Rectangular(5, 12, 8.5, 7), Rectangle_);
+        assertNotEquals(new Rectangular(3, 4, 8.5, 7), Rectangle_);
+    }
+
+    @Test
+    void testToString() {
+        assertEquals("Rectangular {height = 8.5, width = 7.0}", Rectangle_.toString());
+        assertNotEquals("Rectangular {height = 8.5, width = 7}", Rectangle_.toString());
+    }
+
+    @Test
+    void testEquals() {
+        assertEquals(new Rectangular(4, 9, 8.5, 7), Rectangle_);
+        assertNotEquals(new Rectangular(52, 9, 8.5, 7), Rectangle_);
+    }
+
+}
+
+
+
